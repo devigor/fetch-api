177f190bdf13421219e1fe89141a1e64582030f4
Igor Oliveira <devigor.tere@gmail.com>
2017-09-03 09:33:31 -0300

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const cep = document.getElementById('cep');
+const btn = document.getElementById('btn-search');
+const renderInfo = document.getElementById('info');
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault()
+  const cepValue = cep.value;
+  consultaCep(cepValue)
+});
+
+function consultaCep(cep) {
+  const URL = `https://cep.webschool.io/api/br/${cep}`
+  const req = fetch(URL)
+
+  req
+    .then(data => data.json())
+    .then(data => {
+      const { city, address, district, state } = data;
+      createSectionInfo(city, address, district, state);
+    });
+}
+
+function createSectionInfo(city, address, district, state) {
+  const tableInfo = `
+    <table class="table">
+      <thead>
+        <tr>
+          <th>RUA</th>
+          <th>BAIRRO</th>
+          <th>CIDADE</td>
+          <th>ESTADO</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>${address}</td>
+          <td>${district}</td>
+          <td>${city}</td>
+          <td>${state}</td>
+        </tr>
+      </tbody>
+    </table>`
+  renderInfo.innerHTML = tableInfo;
+}
